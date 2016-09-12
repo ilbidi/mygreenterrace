51a3a9ce8d0a828fad21394f370019d4ae48c6e5
ilbidi <fbidinotto@gmail.com>
2016-09-12 10:35:41 +0200

    Chap 4

--- app/models.py
+++ app/models.py
@@ -2,10 +2,10 @@
 from app import db
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	nickname = db.Column(db.String(64), index=True, unique=True)
-	email = db.Column(db.String(256), index=True, unique=True)
-	posts = db.relationship('Post', backref='author', lazy='dynamic')
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(256), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     @property
     def is_authenticated(self):
@@ -25,8 +25,8 @@ class User(db.Model):
         except NameError:
             return str(self.id)  # python 3
 
-	def __repr__(self):
-		return '<User %r>' % self.nickname
+    def __repr__(self):
+        return '<User %r>' % self.nickname
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
